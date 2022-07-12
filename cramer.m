02cb0a7d9325f2d3e19b2f498c2cb30d3345b013
d0gge <ejs.defolt@gmail.com>
2022-07-12 14:28:02 +0500

    ...

--- cramer.m
+++ cramer.m
@@ -1,10 +1,10 @@
 clc;clear;
-matrix = [3,-1,3;4,-1,5;2,-2,4];
-rhs = [5, 4, 6];
+matrix = input("Enter a matrix: ");
+rhs = input("Enter free coefficients: ");
 methods = solution_methods();
 utils = utils();
 fprintf("Solving linear equations " + ...
     "system using Cramer's rule.\n\n");
 [solution_set, enum] = methods.SolveCramer(matrix, rhs);
-fprintf("Solution set #2:\n");
+fprintf("Solution set:\n");
 utils.PrintSolution(solution_set, enum);
--- gauss.m
+++ gauss.m
@@ -1,10 +1,10 @@
 clc;clear;
-matrix = [3,-1,3;4,-1,5;2,-2,4];
-rhs = [5, 4, 6];
+matrix = input("Enter a matrix: ");
+rhs = input("Enter free coefficients: ");
 methods = solution_methods();
 utils = utils();
 fprintf("Solving linear equations " + ...
     "system using Gaussian eliminations.\n\n");
 [solution_set, enum] = methods.SolveGauss(matrix, rhs);
-fprintf("Solution set #3\n");
+fprintf("Solution set:\n");
 utils.PrintSolution(solution_set, enum);
